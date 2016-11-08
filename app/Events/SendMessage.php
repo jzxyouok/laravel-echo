adb298e40f06316a542e79e98bb58f7764b14092
Pedro Lazari <plazari96@gmail.com>
2016-11-08 17:29:44 -0200

    Criando evento

---
+++ app/Events/SendMessage.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pedro Lazari
+ * Date: 08/11/2016
+ * Time: 17:23
+ */
+
+namespace App\Events;
+
+
+use App\Message;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Auth;
+
+class SendMessage implements ShouldBroadcast
+{
+
+    use SerializesModels;
+
+    public $message;
+    public $user;
+
+    public function __construct(Message $message)
+    {
+        $this->user = Auth::user();
+        $this->message = $message;
+    }
+
+    public function broadcastOn()
+    {
+        return new Channel("room.{$this->message->room_id}")
+    }
+
+}
